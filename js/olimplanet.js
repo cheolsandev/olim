ef18d908e00db1a32c96b41e5e170b82c12ab6eb
catteineHong <catteine@gmail.com>
2020-09-30 21:20:32 +0900

    올림 플래닛 - 커밋

--- js/olimplanet.js
+++ js/olimplanet.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var Olim = {};
+
+
+Olim.headerFunc = function(){
+  var $header = $('header'),
+  $gnb = $header.find('.gnb');
+
+  $('.header-btn-nav').on('click', function(){
+    $header.toggleClass('header-nav-on');
+    $gnb.find('.sub').removeClass('on');
+  });
+
+  $gnb.find('> li > a').on('click', function(event){
+    if ($header.hasClass('header-nav-on')) {
+      var $sub = $(this).parent('li').find('.sub');
+      if ($sub.length > 0) {
+       event.preventDefault();
+       $gnb.find('> li').removeClass('on');
+       $sub.closest('li').addClass('on');
+      }
+    }
+  });
+  
+};
+
+Olim.init = function(){
+  // common
+  Olim.headerFunc();
+};
+
+Olim.init();
